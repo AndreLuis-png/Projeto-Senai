0ae39cb857ec29756fd9b9473ebfd6fbf90d56b9
andre <cardanzao1213@gmail.com>
2026-06-26 14:15:33 -0300

    enchendo o banco de linguiça

--- static/css/cantinho_do_castigo/Banco-de-dados.sql
+++ static/css/cantinho_do_castigo/Banco-de-dados.sql
@@ -68,6 +68,23 @@ UPDATE usuarios
 SET senha = '$2a$12$TyKbVE6G425lA7ko/IgwoOcR.Uc4RCbcvGj/ftZkopSNhhlelM8Zi'
 WHERE login = 'admin';
 
+INSERT INTO usuarios (login, senha, status, role) #Adiciona usuários; senha: delicinhas
+VALUES ('Joao Camara', '$2a$12$3IfgDTjfMRr7yQN1nrWC9.PaciUGyIv4tCZXQ3iZisGuSiurJu7.O', 'ativo', 'user');
+INSERT INTO usuarios (login, senha, status, role) #Adiciona usuários; senha: delicinhas
+VALUES ('Matheus', '$2a$12$BHll1yOD6XtedIS1RF4BkOa2tCspibhRKPGFgz.KD1kI3W1/ocFTm', 'ativo', 'user');
+INSERT INTO usuarios (login, senha, status, role) #Adiciona usuários; senha: delicinhas
+VALUES ('Luam', '$2a$12$q8ZhuyPclUKt8AoF7vsSg.d2oYBiXMItNB9EMOuuj43kBbEHLeZ1S', 'ativo', 'user');
+
+INSERT INTO estoque (id_produto, nome, area, quantidade, descricao, link_midia) #Adiciona itens
+VALUES ('00001', 'Alicate', 'Geral', 10, 'Aperta umas coisa ai...', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS66xbAeYwltYSUHqGq4qKWALJX3lkY1ojqbRLkKs82Yw&s=10');
+INSERT INTO estoque (id_produto, nome, area, quantidade, descricao, link_midia) #Adiciona itens
+VALUES ('10001', 'Pregos', 'Mecânica', 200, 'Entra reto', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuleVPCTMLQEhf4lkkzlW9AEMXDfDEqw4RAwsZjdZ-jw&s=10');
+INSERT INTO estoque (id_produto, nome, area, quantidade, descricao, link_midia) #Adiciona itens
+VALUES ('10002', 'Parafusos', 'Mecânica', 200, 'Entra rodando', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlm_v8Khvn-wORVgWlbuepAl0Urz62I7pJCK2UQ3-nnQ&s=10');
+INSERT INTO estoque (id_produto, nome, area, quantidade, descricao, link_midia) #Adiciona itens
+VALUES ('20001', 'Paineis fotovoltaicos', 'Elétrica', 2, 'Deixa o wifi ligado', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMI9NnZ9OYWpmM1wtQ-M1_qA-3j2069zxWJkRJ1pBIew&s=10');
+INSERT INTO estoque (id_produto, nome, area, quantidade, descricao, link_midia) #Adiciona itens
+VALUES ('00002', 'Chave philips', 'Geral', 7, 'Descrição curta do item', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMOMqwTSGR4S2soHpfyq7s5czjFTB6h6zBHHycqd2ZRg&s=10');
 
 SELECT * FROM estoque;
 SELECT * FROM config_admin;
